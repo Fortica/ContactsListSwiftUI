fc2912d4c54a51bc35b9462b7f6a73bdde645864
Larisa Marcinevskaya <marcinevskaya93@mail.ru>
2021-06-21 00:02:42 +0300

    Initial Commit

---
+++ ContactsListSwiftUI/ContactsListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsListSwiftUIApp.swift
+//  ContactsListSwiftUI
+//
+//  Created by Larisa on 21.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactsListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
